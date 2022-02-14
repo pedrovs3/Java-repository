c153a605b246615530e7468d004f11828b1f6e9b
Pedro Vieira <86010036+pedrovs3@users.noreply.github.com>
2022-02-14 11:22:59 -0300

    Add files via upload

--- Exec.java
+++ Exec.java
@@ -8,7 +8,7 @@ class Exec{
         double multiplicacao = decimal1 * decimal2;
         double divisao = decimal1 / decimal2;
         
-        System.out.println("A soma dos valores é: " + soma + " A Subtração dos valores é: " + subtracao + " A multiplicação dos valores é: " + multiplicacao + " A Divisao dos valores é: " + divisao);    
+        System.out.println("A soma dos valores é: " + soma + "\nA Subtração dos valores é: " + subtracao + "\nA multiplicação dos valores é: " + multiplicacao + "\nA Divisao dos valores é: " + divisao);    
     }
 
     static void ex02(){
@@ -23,16 +23,51 @@ class Exec{
         double raiz1 = Math.sqrt(num1);
         double raiz2 = Math.sqrt(num2);
 
-        System.out.println("Exponum1" + valorexpo1 + "Exponum2" + valorexpo2 + "divisão" + divisao + "modulo" + modulo + "Raiz numero1" + raiz1 + "Raiz numero 2" + raiz2);
+        System.out.print("Exponum1: " + valorexpo1 + "\nExponum2: " + valorexpo2 + "\nDivisão: " + divisao + "\nMódulo: " + modulo + "\nRaiz numero1" + raiz1 + "\nRaiz numero 2" + raiz2);
     }
 
     static void ex03(){
-        
+        int var1 = 10;
+        int var2 = 20;
+
+        int cache = var1;
+        var1 = var2;
+        var2 = cache;
+
+        System.out.print("\nA variavel 1 tinha o valor: 10 e após a troca ficou com: " + var1 + "\nJá a variavel 2 tinha o valor: 20 e agora possui: " + var2);
+    }
+
+    static void ex04(){
+        double compra = 120;
+        double desconto = 30;
+
+        double valorDesconto = (compra / 100) * desconto;
+        double valorFinal = compra - valorDesconto;
+        System.out.print("\nO valor da compra com desconto fica: " + valorFinal);
+    }
+
+    static void ex05(){
+        double valorDol = 100;
+        double valorReal = 5.25;
+
+        double conversao = valorDol * valorReal;
+        System.out.print("\nO valor de " + valorDol + " dolares em reais fica: " + conversao);
+    }
+
+    static void ex06(){
+        int celsius = 20;
+
+        int f = (9 * celsius + 160) / 5;
+        System.out.print("\nA temperatura de " + celsius + "°C em Fahreinheit é: " + f + "°F");
+
     }
 
     public static void main(String[] args) {
     Ex01();
     ex02();
-
+    ex03();
+    ex04();
+    ex05();
+    ex06();
     }
 }
