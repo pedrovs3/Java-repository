3a7506eeaa429c3a62bfa81393b94bc015cb26e5
Pedro Vieira <86010036+pedrovs3@users.noreply.github.com>
2022-02-16 10:12:13 -0300

    Add files via upload

--- Gasto.java
+++ Gasto.java
@@ -1,14 +1,18 @@
+import java.io.Console;
+
 class Gasto{
     public static void main(String args[]) {
-        double tempo = 4;
-        int velocidade = 100;
+        
+        Console line = System.console();
+        String tempo = line.readLine("Digite por quantas horas voce dirigiu: ");
+        String velocidade = line.readLine("Digite a velocidade média durante a viagem: ");
 
-        double distancia = tempo * velocidade;
+        double distancia = Double.parseDouble(tempo) * Double.parseDouble(velocidade);
 
         double LITROS_USADOS = distancia / 12;
         LITROS_USADOS = Math.round(LITROS_USADOS);
 
-        System.out.println("A velocidade média do veículo é: " + velocidade + "KM/H" + "\nO tempo gasto foi de: " + tempo + " Horas" + "\nA distância percorrida foi: " + distancia +"KM" + "\nA quantidade de litro gasto na viagem foi de: " + LITROS_USADOS + "L");
+        System.out.println("A velocidade média do veículo é: " + velocidade + "KM/H" + "\nO tempo gasto foi de: " + tempo + " Horas" + "\nA distância percorrida foi: " + distancia +"KM" + "\nA quantidade de litros gasto na viagem foi de: " + LITROS_USADOS + "L");
     }
 }
 
