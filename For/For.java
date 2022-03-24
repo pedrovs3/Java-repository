c11300c20e29998e26497e806f4a7631a6dc0e25
Pedro Vieira <86010036+pedrovs3@users.noreply.github.com>
2022-03-24 10:28:05 -0300

    Add files via upload

---
+++ For/For.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class For {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        for (int i = 20; i <= 50; i+=1){
+            System.out.print(i + ", ");
+        }
+        
+        System.out.println("\nDeseja continuar? [1(Sim)/2(Nao)]");
+        Short cont = ler.nextShort();
+
+        if (cont == 1) {
+            System.out.print("Mais quantos valores? ");
+            int val = ler.nextInt();
+
+            for (int i = 50; i <= 51 -1 + val; i+=1){
+                System.out.print(i + ", ");
+            }
+        }
+        else{
+            System.out.print("Programa Finalizado.");
+        }
+    }
+}
