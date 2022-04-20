5001a1ac3f14a4ffdfdb3eca823baf5bf7136a74
Pedro Vieira <86010036+pedrovs3@users.noreply.github.com>
2022-04-20 11:13:45 -0300

    Add files via upload

--- metodos/Calculadora.java
+++ metodos/Calculadora.java
@@ -1,22 +1,15 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 class Calculador{
-    public static Scanner ler = new Scanner(System.in);
+    static Scanner ler = new Scanner(System.in);
     public static void main(String args[]){
-        
-        double[] values = entrada();
-        double n1 = values[0];
-        double n2 = values[1];
-        
-        
+        saida(entrada());
+    }
 
-        double resultSoma = somar(n1, n2);
-        double resultSub = subtrair(n1, n2);
-        double resultMulti = multiplicar(n1, n2);
-        double resultDiv = dividir(n1, n2);
-
-        System.out.printf("Operações com %.1f e %.1f \nSoma = %.1f \nSubtração = %.1f \nMultiplicação = %.1f \nDivisao = %.1f", n1, n2, resultSoma, resultSub, resultMulti, resultDiv);
+    public static double entrada1(){
+        System.out.print("Digite o primeiro valor:");
+        double n1 = ler.nextDouble();
+        return n1;
     }
 
     public static double[] entrada(){
@@ -27,6 +20,13 @@ class Calculador{
         double[] valores = {n1, n2};
 
         return valores;
+    }
+
+    public static void saida(double[] valores){
+        double n1 = valores[0];
+        double n2 = valores[1];
+
+        System.out.printf("Operações com %.1f e %.1f \nSoma = %.1f \nSubtração = %.1f \nMultiplicação = %.1f \nDivisao = %.1f", n1, n2, somar(n1, n2), subtrair(n1, n2), multiplicar(n1, n2), dividir(n1, n2));
     }
 
     public static double somar(double n1, double n2){
