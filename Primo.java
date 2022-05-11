c36c0788fec775c590d845c91e1b3abc2b3bc73a
Pedro Vieira <pedrovs3@hotmail.com>
2022-05-11 11:26:42 -0300

    Add files via upload

--- Primo.java
+++ Primo.java
@@ -2,21 +2,43 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Primo {
-    public static void main(String[] args) {
+    public static int entrada() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Digite um valor:");
+        System.out.println("Digite um valor inteiro maior que 1:");
         int n = input.nextInt();
-        ArrayList<Integer> lista = new ArrayList<Integer>();
-        
-        for (int i = 2; i <= n/2; i++) {
-            if(n % i == 0){
-                //System.out.printf("O numero nao é primo pois é divisivel por %s\n", i);
+
+        return n;
+    }
+
+    public static ArrayList divisores(int n, ArrayList<Integer> lista) {
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                // System.out.printf("O numero nao é primo pois é divisivel por %s\n", i);
                 lista.add(i);
             }
-        }
+        }   
 
-        for(int i = 0; i < lista.size(); i++){
-            System.out.println(lista.get(i));
+        return lista;
+    }
+
+    public static void exibir(ArrayList<Integer> lista) {
+        if (lista.size() < 1) {
+            System.out.println("O numero é primo");
+        } else {
+            System.out.println("O numero é divisivel por: ");
+            for (int i = 0; i < lista.size(); i++) {
+                if (lista.size() >= 1) {
+                    System.out.println(lista.get(i));
+                }
+            }
         }
+    }
+    public static void main(String[] args) {
+        int n = entrada();
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+
+        divisores(n, lista);
+
+        exibir(lista);
     }
 }
